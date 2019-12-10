55269586152cf67a114a38732536a17e7a258331
David Markel <davidgmarkel@gmail.com>
2019-12-09 23:48:31 -0500

    add validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :email, presence: true, on: :create
+  validates :password, presence: true, on: :create
+  validates :name, presence: true, on: :update
+  has_secure_password
 end
