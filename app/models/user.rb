c736d0c18bc9ed2b7b024cb999cb4aff6bbdb6d7
David Markel <davidgmarkel@gmail.com>
2019-12-11 18:43:41 -0500

    add foreign key associating posts to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   validates :email, uniqueness:true, on: :create
   validates :password, presence: true, on: :create
   validates :name, presence: true, on: :update
+  has_many :posts
   has_secure_password
 end
