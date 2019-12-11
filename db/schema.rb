77af7f627096ea44e345262be21d6811a09a4ccb
David Markel <davidgmarkel@gmail.com>
2019-12-11 18:50:24 -0500

    drop db, schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_11_233613) do
+ActiveRecord::Schema.define(version: 2019_12_10_044406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "header_image"
-    t.string "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -35,5 +25,4 @@ ActiveRecord::Schema.define(version: 2019_12_11_233613) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "posts", "users"
 end
