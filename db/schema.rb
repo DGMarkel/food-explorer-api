90f5244a9c4c2d079f15e9c326dbf2d8a7c37dc4
David Markel <davidgmarkel@gmail.com>
2019-12-11 19:39:15 -0500

    add columns for bio and profile image to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_000153) do
+ActiveRecord::Schema.define(version: 2019_12_12_003728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 2019_12_12_000153) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "bio"
+    t.string "profile_image"
   end
 
   add_foreign_key "posts", "users"
