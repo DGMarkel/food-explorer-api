45f5530506a28db6e702c39909b34434917e5a53
David Markel <davidgmarkel@gmail.com>
2019-12-11 19:58:07 -0500

    add nested resource user_posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,22 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+    render json: @posts
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    render json: @post
+  end 
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
