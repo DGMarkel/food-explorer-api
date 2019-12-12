3128816a18d04505b16bc7efc53c852e8e9d6f73
David Markel <davidgmarkel@gmail.com>
2019-12-11 19:33:12 -0500

    add update action to User controller, add user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,18 @@ class UsersController < ApplicationController
   def create
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+
   def destroy
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
 
 end
